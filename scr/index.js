56596318c256ee1fac7f28a391aa57e29152c7b3
Olga <olga.dethlefsen@nbis.se>
2026-01-20 12:26:19 +0100

    Add basic js form handling

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,24 @@
 console.log("Hello, World!");
+
+function generatePoem(event) {
+  event.preventDefault();
+  console.log("Generating poem...");
+
+  //   let poemElement = document.querySelector("#poem");
+  //   poemElement.innerHTML =
+  // "Roses are red,\nViolets are blue,\nThis is a poem,\nJust for you.";
+
+  const typewriter = new Typewriter("#poem", {
+    autoStart: true,
+  });
+
+  typewriter
+    .typeString("Roses are red,")
+    .typeString("<br>Violets are blue,")
+    .typeString("<br>This is a poem,")
+    .typeString("<br>Just for you.")
+    .start();
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
