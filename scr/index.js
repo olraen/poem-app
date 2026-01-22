455dcdcb76a215937c738d25a141951221f28dde
Olga <olga.dethlefsen@nbis.se>
2026-01-22 11:26:25 +0100

    Add API poem generating

--- scr/index.js
+++ scr/index.js
@@ -1,23 +1,25 @@
-console.log("Hello, World!");
+function displayPoem(response) {
+  const typewriter = new Typewriter("#poem", {
+    autoStart: true,
+    delay: 30,
+  });
+
+  typewriter.typeString(response.data.answer).start();
+}
 
 function generatePoem(event) {
   event.preventDefault();
   console.log("Generating poem...");
 
-  //   let poemElement = document.querySelector("#poem");
-  //   poemElement.innerHTML =
-  // "Roses are red,\nViolets are blue,\nThis is a poem,\nJust for you.";
+  let poemTopic = document.querySelector("#input-text");
+  let prompt = `Write a short poem about ${poemTopic.value}`;
+  let context =
+    "The poem should be in imabic tetrameter and have a rhyming scheme of AABB, with two stanzas. Separate each line with a <br> tag and separate stanzas with an extra <br> tag. Sign the peom at the bottom with '- ChatGPT'. Make sure to follow the instructions";
 
-  const typewriter = new Typewriter("#poem", {
-    autoStart: true,
-  });
+  let apiKey = "1o02ad6aa40e7b19fa37f8t5928ba37d";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  typewriter
-    .typeString("Roses are red,")
-    .typeString("<br>Violets are blue,")
-    .typeString("<br>This is a poem,")
-    .typeString("<br>Just for you.")
-    .start();
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form");
